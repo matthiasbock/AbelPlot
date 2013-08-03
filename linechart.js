474d0ca20b0bdc32ba4ebb4ace186da417d894e4
Matthias <mail@matthiasbock.net>
2013-08-03 19:01:49 +0200

    objectified; works

--- linechart.js
+++ linechart.js
@@ -1,12 +1,13 @@
+
 LineChart = function() {
-
-    var LineChart = {};
-
+    
+    var Chart = {};
+    
     //
     // Import data:
     // Create arrays from the tab-separated input values
     //
-    LineChart.importTSV = function(elementID) {
+    Chart.importTSV = function(elementID) {
         // the raw text from the specified script element is parsed as tab-separated value table
         var input = document.getElementById(elementID).innerHTML.trim();
         var tsv = d3.tsv.parseRows(input);
@@ -17,20 +18,25 @@ LineChart = function() {
         this.inputDataTable = Array(16);
 
         // thereby each array of columns element is an array of rows
-        for (var i=1; i<=16; i++) this.inputDataTable[i] = [];
+        for (var i=1; i<=16; i++)
+            this.inputDataTable[i] = [];
         
         // the first table column contains the hours, therefore the rows are addressed accordingly
         // and set to the float value of the input table at this position
-        data.forEach(function(row) {
-                    hour = row[0];
-                    for (var i=1; i<=16; i++) this.inputDataTable[i][hour] = parseFloat(row[i]);
-                    });
-        };
+        my = this; // because "this" in the for loop below doesn't refer to "Chart"
+        data.forEach(
+            function(row) {
+                hour = row[0];
+                for (var i=1; i<=16; i++)
+                    my.inputDataTable[i][hour] = parseFloat(row[i]);
+            }
+        );
+    };
 
     //
     // Create Scalable Vector Graphic
     //
-    LineChart.createSVG = function() {
+    Chart.createSVG = function() {
         //
         // Insert SVG below HTML BODY
         //
@@ -46,9 +52,9 @@ LineChart = function() {
         //
         // Create X axis
         //
-        var x = d3.scale.linear().range([0, width]).domain([0, 24]);
+        this.x = d3.scale.linear().range([0, width]).domain([0, 24]);
         var xAxis = d3.svg.axis()
-            .scale(x)
+            .scale(this.x)
             .orient("bottom");
         this.SVG.append("g")
             .attr("class", "x axis")
@@ -61,9 +67,9 @@ LineChart = function() {
         //
         // Create Y axis
         //
-        var y = d3.scale.linear().range([height, 0]).domain([0, 0.5]);
+        this.y = d3.scale.linear().range([height, 0]).domain([0, 0.5]);
         var yAxis = d3.svg.axis()
-            .scale(y)
+            .scale(this.y)
             .orient("left");
         this.SVG.append("g")
             .attr("class", "y axis")
@@ -80,30 +86,30 @@ LineChart = function() {
     // Create an SVG path defining string
     // http://www.janwillemtulp.com/2011/04/01/tutorial-line-chart-in-d3/
     //
-    var arrayToPath = function(data) {
-        result = "M"+x(0)+","+y(data[0]);
+    Chart.arrayToPath = function(data) {
+        result = "M"+this.x(0)+","+this.y(data[0]);
         for (var i=1; i<=24; i++)
             if (typeof(data[i]) != typeof(undefined))
-                result += "L"+x(i)+","+y(data[i]);
-    //    console.log(result);
+                result += "L"+this.x(i)+","+this.y(data[i]);
+        //    console.log(result);
         return result;
-        };
+    };
 
     //
     // Draw a line into the chart
     //
-    LineChart.plotArray = function(data) {
+    Chart.plotArray = function(data) {
         this.SVG.append("path")
     //        .attr("id", "concentration"+index)
             .attr("class", "line")
-            .attr("d", arrayToPath(data));
+            .attr("d", this.arrayToPath(data));
     
         for (var i=1; i<=24; i++)
             if (typeof(data[i]) != typeof(undefined))
                 this.SVG.append("rect")
                     .attr("class", "rect")
-                    .attr("x", x(i)-3)
-                    .attr("y", y(data[i])-3)
+                    .attr("x", this.x(i)-3)
+                    .attr("y", this.y(data[i])-3)
                     .attr("width", "6")
                     .attr("height", "6");
         };
@@ -111,10 +117,10 @@ LineChart = function() {
     //
     // Draw all experiments
     //
-    LineChart.plotAllDataColumns = function() {
+    Chart.plotAllDataColumns = function() {
         for (var i=1; i<=16; i++)
-            plotArray(this.inputDataTable[i]);
+            this.plotArray(this.inputDataTable[i]);
     };
 
-    return LineChart;
-}();
+    return Chart;
+};
